27a90264e17f86bc83a7f1143b1f327df596acd4
Todd <github@christensen.wtf>
2024-11-19 12:50:57 -0800

    update trip controller with locations

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -9,12 +9,11 @@ class TripController extends Controller
 {
     public function index(Request $request)
     {
-        // $driver_id = $request->user()->driver->id;
-        // $trips = Trip::where('driver_id', $driver_id)
-        //     ->orderBy('created_at', 'desc')
-        //     ->get();
-
-        $trips = Trip::orderBy('created_at', 'desc')->get();
+        $driver_id = $request->user()->driver->id;
+        $trips = Trip::where('driver_id', $driver_id)
+            ->orderBy('created_at', 'desc')
+            ->with('locations')
+            ->get();
 
         return response()->json($trips);
     }
