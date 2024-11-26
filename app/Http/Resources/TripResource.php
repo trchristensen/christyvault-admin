9313ab4d569f4d6cf9a813298b2496249c0a4c9c
Todd <github@christensen.wtf>
2024-11-26 12:57:33 -0800

    api trip resource for trip controller

--- app/Http/Resources/TripResource.php
+++ app/Http/Resources/TripResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class TripResource extends JsonResource
 {
