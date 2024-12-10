5dfc37556287e6a14af2a04b80a7e5889f142ed0
= <github@christensen.wtf>
2024-12-10 10:06:44 -0800

    remove root path

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/orders/{order}/print', [DeliveryTagController::class, 'view'])->name('orders.print');
 
