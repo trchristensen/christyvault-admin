4bf952309e1352d8fd585a140b2674afcb191329
Todd Christensen <github@christensen.wtf>
2025-01-27 14:46:04 -0800

    add uof to scanned screen next to qty input

--- resources/views/kanban-cards/scan.blade.php
+++ resources/views/kanban-cards/scan.blade.php
@@ -50,9 +50,16 @@
                             <label class="block font-medium text-gray-700">
                                 Remaining Quantity (Optional)
                             </label>
-                            <input type="number" name="remaining_quantity" step="0.01"
-                                class="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                                placeholder="Enter remaining quantity">
+                           
+                                <!-- add unit of measure next to input -->
+                                 <div class="flex justify-between">
+                                    <input type="number" name="remaining_quantity" step="0.01"
+                                        class="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                                        placeholder="Enter remaining quantity">
+                                    <p class="mt-1 text-sm text-gray-500">
+                                        {{ $kanbanCard->unit_of_measure }}
+                                    </p>
+                                </div>
                             <p class="mt-1 text-sm text-gray-500">
                                 Leave blank if you don't want to report the quantity
                             </p>
