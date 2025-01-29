1cfd1ca0d6ee6c98ea3a4959b203720827d92324
Todd Christensen <github@christensen.wtf>
2025-01-29 08:10:59 -0800

    improve po item select

--- app/Filament/Operations/Resources/PurchaseOrderResource/RelationManagers/InventoryItemsRelationManager.php
+++ app/Filament/Operations/Resources/PurchaseOrderResource/RelationManagers/InventoryItemsRelationManager.php
@@ -105,7 +105,7 @@ class InventoryItemsRelationManager extends RelationManager
                     ->form(fn(Tables\Actions\AttachAction $action): array => [
                         $action->getRecordSelect()
                             ->label('Inventory Item')
-                            ->options(fn(Get $get): Collection => InventoryItem::query()
+                            ->options(fn(): Collection => InventoryItem::query()
                                 ->with(['suppliers' => function($query) {
                                     $query->select('suppliers.id', 'suppliers.name')
                                         ->wherePivot('is_preferred', '=', DB::raw('true'))
@@ -124,10 +124,6 @@ class InventoryItemsRelationManager extends RelationManager
                                 })
                                 ->select('inventory_items.*')
                                 ->orderByRaw('CASE WHEN inventory_item_suppliers.supplier_id IS NOT NULL THEN 0 ELSE 1 END')
-                                ->where(function($query) use ($get) {
-                                    $query->where('sku', 'like', '%' . $get('sku') . '%')
-                                          ->orWhere('name', 'like', '%' . $get('name') . '%');
-                                })
                                 ->get()
                                 ->mapWithKeys(function ($item) {
                                     $preferredSupplier = $item->suppliers->first();
@@ -144,8 +140,49 @@ class InventoryItemsRelationManager extends RelationManager
                                                     '</div>'
                                     ];
                                 }))
-                            ->searchable()
-                            ->preload()
+                            ->getSearchResultsUsing(function (string $search) {
+                                return InventoryItem::query()
+                                    ->with(['suppliers' => function($query) {
+                                        $query->select('suppliers.id', 'suppliers.name')
+                                            ->wherePivot('is_preferred', '=', DB::raw('true'))
+                                            ->withPivot('supplier_sku');
+                                    }])
+                                    ->leftJoin('inventory_item_suppliers', function($join) {
+                                        $join->on('inventory_items.id', '=', 'inventory_item_suppliers.inventory_item_id')
+                                            ->where('inventory_item_suppliers.supplier_id', '=', $this->getOwnerRecord()->supplier_id)
+                                            ->where('inventory_item_suppliers.is_preferred', '=', DB::raw('true'));
+                                    })
+                                    ->whereNotExists(function ($query) {
+                                        $query->select(DB::raw(1))
+                                            ->from('purchase_order_items')
+                                            ->whereColumn('purchase_order_items.inventory_item_id', 'inventory_items.id')
+                                            ->where('purchase_order_items.purchase_order_id', $this->getOwnerRecord()->id);
+                                    })
+                                    ->where(function($query) use ($search) {
+                                        $query->where('sku', 'ilike', "%{$search}%")
+                                              ->orWhere('name', 'ilike', "%{$search}%")
+                                              ->orWhere('inventory_item_suppliers.supplier_sku', 'ilike', "%{$search}%");
+                                    })
+                                    ->select('inventory_items.*')
+                                    ->orderByRaw('CASE WHEN inventory_item_suppliers.supplier_id IS NOT NULL THEN 0 ELSE 1 END')
+                                    ->limit(50)
+                                    ->get()
+                                    ->mapWithKeys(function ($item) {
+                                        $preferredSupplier = $item->suppliers->first();
+                                        $supplierInfo = $preferredSupplier 
+                                            ? $preferredSupplier->name . 
+                                              ($preferredSupplier->pivot->supplier_sku ? ' (' . $preferredSupplier->pivot->supplier_sku . ')' : '')
+                                            : '';
+
+                                        return [
+                                            $item->id => '<div style="line-height: 1.2;">
+                                                            <span style="font-size: 0.9em; font-weight: bold;">' . $item->sku . '</span><br>' . 
+                                                            $item->name . 
+                                                            ($supplierInfo ? '<br>' . $supplierInfo : '') .
+                                                        '</div>'
+                                        ];
+                                    });
+                            })
                             ->allowHtml(),
                         Forms\Components\TextInput::make('supplier_sku')
                             ->label('Supplier SKU')
