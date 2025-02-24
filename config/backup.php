ec28acee1d79d2474c974978a59e3d96492835ff
Todd Christensen <github@christensen.wtf>
2025-02-24 10:25:34 -0800

    changed backup to r2..

--- config/backup.php
+++ config/backup.php
@@ -153,7 +153,7 @@ return [
              * The disk names on which the backups will be stored.
              */
             'disks' => [
-                'local',
+                'r2',
             ],
         ],
 
